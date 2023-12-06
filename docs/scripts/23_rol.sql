bedaf59db72e761e621ec91d997dc04ba07a8295
arstro <lil_ham0n@hotmail.com>
2023-12-06 00:39:17 -0600

    Update 23_rol.sql

--- docs/scripts/23_rol.sql
+++ docs/scripts/23_rol.sql
@@ -3,7 +3,10 @@ ALTER TABLE `usuario` CHANGE `usertipo` `usertipo` CHAR(5) CHARACTER SET utf8 CO
 
 
 /**/
+USE skull2canldydb;
 UPDATE `usuario` SET `usertipo` = 'ADMIN' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
+
+/*No pago subscripcion*/
 UPDATE `usuario` SET `usertipo` = 'CLN' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
 
 UPDATE `usuario` SET `usertipo` = 'CLS' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
