2b4b6ebdd5f11cef32d088809b90a2530279cc97
arstro <lil_ham0n@hotmail.com>
2023-12-06 00:26:33 -0600

    PULL

---
+++ docs/scripts/23_rol.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `usuario` CHANGE `usertipo` `usertipo` CHAR(5) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'Tipo de Usuario, Normal, Consultor o Cliente';
+
+
+
+/**/
+UPDATE `usuario` SET `usertipo` = 'ADMIN' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
+UPDATE `usuario` SET `usertipo` = 'CLN' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
+
+UPDATE `usuario` SET `usertipo` = 'CLS' WHERE `usuario`.`usercod` = 'f0fef9d5e79c8d40a320ec4eb39936c3eaa035aaf0c46b7d118dead4294fb764';
