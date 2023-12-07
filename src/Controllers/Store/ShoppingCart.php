9851f2eb9867e09a28a3305f0a77acb1eab8113e
arstro <lil_ham0n@hotmail.com>
2023-12-07 16:33:13 -0600

    Revert "Store> y shoppingCart>"
    This reverts commit bd4d6c229a4369a103c0d1389bf11956757ccebe.

--- src/Controllers/Store/ShoppingCart.php
+++ src/Controllers/Store/ShoppingCart.php
@@ -24,7 +24,7 @@ class ShoppingCart extends PublicController
         self::carrito();
         $fecha_mas_reciente_timestamp = 0;
        
-            $xls = \DAO\Security\Security::getCodigoByEmail($_SESSION["login"]['userEmail']); 
+            $xls = \DAO\Security\Security::getCodigoByEmail($_SESSION['useremail']); 
         
         if (isset($_SESSION["cart" . $xls])) {
             $cart = $_SESSION["cart" . $xls];
@@ -96,22 +96,13 @@ class ShoppingCart extends PublicController
 
     private function Carrito()
     {
-        $xls = \DAO\Security\Security::getCodigoByEmail($_SESSION["login"]['userEmail']);
+
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($_SESSION["tokenShopping"] == $_POST["xsx"]) {
                 if (isset($_POST['deleteButton'])) {
-                    if (!isset($_SESSION['cart'.$xls])) {
-                        unset($_SESSION['cart'.$xls]);
+                    if (!isset($_SESSION['cart'])) {
+                        unset($_SESSION['cart']);
                         echo '<script>alert("Carrito was removed");</script>';
-                    }
-                }
-                if(isset($_POST['deleteItem'])){
-                    $crrfching = $_POST['deleteItem'];
-                    foreach ($_SESSION['cart'. $xls] as $key =>$product) {
-                        if ($product['crrfching'] === $crrfching) {
-                            unset($_SESSION['cart'.$xls][$key]);
-                            break; 
-                        }
                     }
                 }
             }
